d4f367925fc837dbcb3ebca4d5aa01189134a7df
c1102821 <adam0103232@gmail.com>
2021-12-06 14:47:57 +0800

    feat: remove some unnecessary files

--- list2dic_def.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-keys = ['a', 'b', 'c']
-
-def list2dic(keys):
-    values = np.arange(keys.__len__()) + 1
-    dic = dict(zip(keys, values))
-    return dic
-
-dic1 = list2dic(keys)
